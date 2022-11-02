ab0de13984b4fd5422c11b92e6bc409d2164eff4
sramk87 <sramkissoon87@gmail.com>
2022-11-02 15:07:53 -0400

    photos rendered for each category

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,12 +1,19 @@
 import React, { useEffect } from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
+
+
 function Nav(props) {
+
   const {
     categories = [],
     setCurrentCategory,
     currentCategory,
   } = props;
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   return (
     <header className="flex-row px-1">
@@ -45,10 +52,6 @@ function Nav(props) {
       </nav>
     </header>
   );
-
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
 }
 
 export default Nav;
